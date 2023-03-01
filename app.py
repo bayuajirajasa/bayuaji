16746c064f49684199bb36cb55c88b93ed40e828
bayuajirajasa <bayugaming140@gmail>
2023-03-01 09:03:08 +0700

    First Commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv(dotenv_path)
 MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME = os.environ.get("DB_NAME")
 
-client = MongoClient('')
+client = MongoClient('MONGODB_URI')
 
 db = client[DB_NAME]
 
